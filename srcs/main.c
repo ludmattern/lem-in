800af6c0edcec76682e0d0c7977018dbce5df6b7
ludmattern <github.rfgrg@passmail.net>
2025-08-03 19:02:41 +0200

    refactor: remove redundant comment to improve code clarity

--- srcs/main.c
+++ srcs/main.c
@@ -8,7 +8,6 @@ int main(void)
 	if (!parser)
 		return EXIT_FAILURE;
 
-	// Read input
 	if (!read_input(parser) || !parse_input(parser) || !valid_path(parser) || !find_paths(parser) || !display_input(parser))
 		status = EXIT_FAILURE;
 
