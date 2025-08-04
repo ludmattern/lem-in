5a2b96cfac8a18b4d0866f078877ff40a09954a5
ludmattern <github.rfgrg@passmail.net>
2025-08-04 14:55:42 +0200

    refactor: improve code formatting and add scaling calculations for visualizer

--- visualizer/src/main.c
+++ visualizer/src/main.c
@@ -2,8 +2,8 @@
 
 Map g_map;
 
-SDL_Surface* screen = NULL;
-TTF_Font* font = NULL;
+SDL_Surface *screen = NULL;
+TTF_Font *font = NULL;
 
 int next_start = 0;
 int next_end = 0;
@@ -11,12 +11,17 @@ int is_first_line = 1;
 
 int current_turn = 0;
 int max_turns = 0;
-char* turn_lines[MAX_ACTIONS_PER_TURN];
+char *turn_lines[MAX_ACTIONS_PER_TURN];
 int turn_line_count = 0;
+
+// Scaling variables
+float scale_factor = 1.0;
+int offset_x = 0, offset_y = 0;
+int window_width = 1200, window_height = 800;
 
 int main(void)
 {
-    init_map();
-    display_map();
-    return (0);
+	init_map();
+	display_map();
+	return (0);
 }
