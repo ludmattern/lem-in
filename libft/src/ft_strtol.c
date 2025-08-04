093a3859be8584f49abc6839861592bb4e1686e1
ludmattern <github.rfgrg@passmail.net>
2025-08-04 10:29:58 +0200

    feat: implement ft_strtol function for string to long conversion

---
+++ libft/src/ft_strtol.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtol.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmattern <lmattern@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/04 10:25:03 by lmattern          #+#    #+#             */
+/*   Updated: 2025/08/04 10:25:03 by lmattern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/libft.h"
+
+long ft_strtol(const char *str, char **endptr, int base)
+{
+	long result;
+	int sign;
+
+	if (!str)
+		return (0);
+	while (*str && ft_isspace(*str))
+		str++;
+	sign = 1;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	result = 0;
+	while (*str && ft_isdigit(*str))
+	{
+		result = result * base + (*str - '0');
+		str++;
+	}
+	if (endptr)
+		*endptr = (char *)str;
+	return (result * sign);
+}
